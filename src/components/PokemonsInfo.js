562a2b41cc86174888f3d9cca15907c8d5bc4867
Brayan Leyva <brayanleyvag@gmail.com>
2022-03-30 19:01:00 -0600

    pokemonInfo styles

--- src/components/PokemonsInfo.js
+++ src/components/PokemonsInfo.js
@@ -12,9 +12,11 @@ export const PokemonsInfo = () => {
         .then(res => setPokemonInfo(res.data))
     }, [])
 
-    function typeColor() {
+    //pokemonInfo.types?.[0].type.name
+
+    function typeColor(type) {
       let color = "white"
-      switch (pokemonInfo.types?.[0].type.name) {
+      switch (type) {
         case 'normal':
           color = "#735259";
           break;
@@ -82,18 +84,21 @@ export const PokemonsInfo = () => {
     }
 
     const fontColor = {
-      color:`${typeColor()}`
+      color:`${typeColor(pokemonInfo.types?.[0].type.name)}`
     }
 
+    function statsPercentage(index) {
+      return `${(100/150)*(pokemonInfo.stats?.[index].base_stat)}%` 
+    }
 
   return (
     <div className='all-detail'>
       <img src={pokemonInfo.sprites?.other["official-artwork"].front_default} alt={`${id} image`}></img>
-      <div className='bg-color-detail' style={{backgroundColor:`${typeColor()}`}}>
+      <div className='bg-color-detail' style={{backgroundColor:`${typeColor(pokemonInfo.types?.[0].type.name)}`}}>
         <div className='bg-white-detail'>
           <div className='detail-content'>
             <span className='id-number' style={fontColor}>#{pokemonInfo.id}</span>
-            <div className='name-separator'>
+            <div className='all-separator'>
               <div className='separator'></div>
               <h3 className='pokemon-name-detail' style={fontColor}>{`${pokemonInfo.forms?.[0].name.charAt(0).toUpperCase()}${pokemonInfo.forms?.[0].name.slice(1)}`}</h3>
               <div className='separator'></div>
@@ -113,62 +118,86 @@ export const PokemonsInfo = () => {
             <div className='type-ab-container'>
               <div className='tyab-container'>
                 <span>Type</span>
-                {pokemonInfo.types?.[1]?.type.name ? <div className='ta-container-details'><span className='ta-boxes'>{`${pokemonInfo.types?.[0].type.name.charAt(0).toUpperCase()}${pokemonInfo.types?.[0].type.name.slice(1)}`}</span><span className='ta-boxes'>{`${pokemonInfo.types?.[1].type.name.charAt(0).toUpperCase()}${pokemonInfo.types?.[1].type.name.slice(1)}`}</span></div>:<span className='ta-boxes'>{`${pokemonInfo.types?.[0].type.name.charAt(0).toUpperCase()}${pokemonInfo.types?.[0].type.name.slice(1)}`}</span>}
+                {pokemonInfo.types?.[1]?.type.name ? <div className='ta-container-details'><span style={{color: 'white',backgroundColor:`${typeColor(pokemonInfo.types?.[0].type.name)}`}} className='ta-boxes'>{`${pokemonInfo.types?.[0].type.name.charAt(0).toUpperCase()}${pokemonInfo.types?.[0].type.name.slice(1)}`}</span><span style={{color: 'white',backgroundColor:`${typeColor(pokemonInfo.types?.[1].type.name)}`}} className='ta-boxes'>{`${pokemonInfo.types?.[1].type.name.charAt(0).toUpperCase()}${pokemonInfo.types?.[1].type.name.slice(1)}`}</span></div>:<span style={{color: 'white',backgroundColor:`${typeColor(pokemonInfo.types?.[0].type.name)}`}} className='ta-boxes'>{`${pokemonInfo.types?.[0].type.name.charAt(0).toUpperCase()}${pokemonInfo.types?.[0].type.name.slice(1)}`}</span>}
               </div>
               <div className='tyab-container'>
                 <span>Abilities</span>
                 {pokemonInfo.abilities?.[1]?.ability.name ? <div className='ta-container-details'><span className='ta-boxes'>{`${pokemonInfo.abilities?.[0].ability.name.charAt(0).toUpperCase()}${pokemonInfo.abilities?.[0].ability.name.slice(1)}`}</span><span className='ta-boxes'>{`${pokemonInfo.abilities?.[1].ability.name.charAt(0).toUpperCase()}${pokemonInfo.abilities?.[1].ability.name.slice(1)}`}</span></div>:<span className='ta-boxes'>{`${pokemonInfo.abilities?.[0].ability.name.charAt(0).toUpperCase()}${pokemonInfo.abilities?.[0].ability.name.slice(1)}`}</span>}
               </div>
             </div>
-            <div>
-              <span>Stats</span>
-              <hr/>
+            <div className='all-separator'>
+              <span className='separator-title'>Stats</span>
+              <div className='separator2'/>
               <div className='pokeball-detail'>
-                <div className='ellipse-5'></div>
-                <div className='ellipse-6'></div>
-                <div className='ellipse-7'></div>
+                <div className='ellipse-5'>
                 <div className='vector-10'></div>
+                  <div className='ellipse-6'>
+                    <div className='ellipse-7'></div>
+                  </div>
                 <div className='vector-11'></div>
+                </div>
               </div>
             </div>
-            <div>
-              <div>
-                <span>HP:</span>
-                <span>{pokemonInfo.stats?.[0].base_stat}</span>
+            <div className='main-stats'>
+              <div className='ind-container'>
+                <div className='ind-stats'>
+                  <span>HP</span>
+                  <span>{pokemonInfo.stats?.[0].base_stat} / 150</span>
+                </div>
+                <div className='bar'>
+                  <div className='ind-bar' style={{width:`${statsPercentage(0)}`}}></div>
+                </div>
               </div>
-              <div>
-                <span>Attack:</span>
-                <span>{pokemonInfo.stats?.[1].base_stat}</span>
+              <div className='ind-container'>
+                <div className='ind-stats'>
+                  <span>Attack</span>
+                  <span>{pokemonInfo.stats?.[1].base_stat} / 150</span>
+                </div>
+                <div className='bar'>
+                  <div className='ind-bar' style={{width:`${statsPercentage(1)}`}}></div>
+                </div>
               </div>
-              <div>
-                <span>Defense:</span>
-                <span>{pokemonInfo.stats?.[2].base_stat}</span>
+              <div className='ind-container'>
+                <div className='ind-stats'>
+                  <span>Defense</span>
+                  <span>{pokemonInfo.stats?.[2].base_stat} / 150</span>
+                </div>
+                <div className='bar'>
+                  <div className='ind-bar' style={{width:`${statsPercentage(2)}`}}></div>
+                </div>
               </div>
-              <div>
-                <span>Speed:</span>
-                <span>{pokemonInfo.stats?.[5].base_stat}</span>
+              <div className='ind-container'>
+                <div className='ind-stats'>
+                  <span>Speed</span>
+                  <span>{pokemonInfo.stats?.[5].base_stat} / 150</span>
+                </div>
+                <div className='bar'>
+                  <div className='ind-bar' style={{width:`${statsPercentage(5)}`}}></div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div>
-        <div>
-          <span>Movements</span>
-          <hr/>
+      <div className='w-container2'>
+        <div className='all-separator'>
+          <span className='separator-title'>Moves</span>
+          <div className='separator2'/>
           <div className='pokeball-detail'>
-            <div className='ellipse-5'></div>
-            <div className='ellipse-6'></div>
-            <div className='ellipse-7'></div>
-            <div className='vector-10'></div>
-            <div className='vector-11'></div>
-          </div>
+              <div className='ellipse-5'>
+              <div className='vector-10'></div>
+                <div className='ellipse-6'>
+                  <div className='ellipse-7'></div>
+                </div>
+              <div className='vector-11'></div>
+              </div>
+            </div>
         </div>
-        <div>
+        <div className='moves-container'>
           {
             pokemonInfo.moves?.map(move => (
-              <span key={move.move.name}>{move.move.name}</span>
+              <span key={move.move.name} className='ind-moves'>{move.move.name}</span>
             ))
           }
         </div>
